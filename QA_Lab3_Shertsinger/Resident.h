a99ea927f62c4868210b107095faf4e2343bc333
Siegward91 <143430182+Siegward91@users.noreply.github.com>
2026-05-05 01:14:22 +0700

    Update Resident.h

--- QA_Lab3_Shertsinger/Resident.h
+++ QA_Lab3_Shertsinger/Resident.h
@@ -1,12 +1,27 @@
+/**
+ * @file Resident.h
+ * @brief Базовый класс жильца дома
+ * @author Я.А. Шерцингер
+ * @version 1.0
+ */
+
 #ifndef RESIDENT_H
 #define RESIDENT_H
 
 #include <string>
 
+/**
+ * @class Resident
+ * @brief Базовый класс, представляющий жильца дома
+ * 
+ * Хранит ФИО и возраст жильца. Является базовым классом
+ * для иерархии наследования. Содержит виртуальные методы
+ * для полиморфного поведения.
+ */
 class Resident {
 protected:
-    std::string fullName;
-    int age;
+    std::string fullName;  ///< Фамилия, имя, отчество жильца
+    int age;               ///< Возраст жильца в годах
 
 public:
     Resident();
@@ -17,7 +32,16 @@ public:
     int getAge() const;
     void setAge(int newAge);
 
+    /**
+     * @brief Проверить, является ли жилец совершеннолетним
+     * 
+     * Совершеннолетие определяется по формуле:
+     * \f$ isAdult = (age \geq 18) \f$
+     * 
+     * @return true если возраст >= 18, иначе false
+     */
     virtual bool isAdult() const;
+
     virtual std::string toString() const;
 };
 
