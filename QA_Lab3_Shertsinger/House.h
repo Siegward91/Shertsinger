9f0de570b2ca0e30452d86eeed0d42924b76472f
Siegward91 <143430182+Siegward91@users.noreply.github.com>
2026-05-05 01:22:04 +0700

    Update House.h

--- QA_Lab3_Shertsinger/House.h
+++ QA_Lab3_Shertsinger/House.h
@@ -1,3 +1,10 @@
+/**
+ * @file House.h
+ * @brief Класс, представляющий дом с жильцами
+ * @author Я.А. Шерцингер
+ * @version 1.0
+ */
+
 #ifndef HOUSE_H
 #define HOUSE_H
 
@@ -5,28 +12,52 @@
 #include "PrivilegedResident.h"
 #include <vector>
 
+/**
+ * @class House
+ * @brief Класс для хранения данных о доме и его жильцах
+ * 
+ * Содержит номер дома и полиморфный список жильцов.
+ * Поддерживает добавление обычных и льготных жильцов,
+ * поиск самого старшего, подсчёт несовершеннолетних.
+ * 
+ * \image html house_structure.png "Структура класса House"
+ */
 class House {
 private:
-    int houseNumber;
-    std::vector<Resident*> residents;
+    int houseNumber;                        ///< Номер дома
+    std::vector<Resident*> residents;       ///< Список жильцов
 
 public:
+    /**
+     * @brief Конструктор
+     * @param number Номер дома
+     */
     House(int number);
+
+    /**
+     * @brief Деструктор, освобождает память
+     */
     ~House();
 
     int getHouseNumber() const;
-
     void addResident(const std::string& name, int age);
-
     void addPrivilegedResident(const std::string& name, int age, 
                                const std::string& category);
-
     int getResidentCount() const;
-
     Resident* getResident(int index) const;
 
+    /**
+     * @brief Найти самого старшего жильца
+     * 
+     * Формула: \f$ maxAge = \max_{i=1}^{n} age_i \f$
+     * @return Указатель на самого старшего жильца
+     */
     Resident* findOldestResident() const;
 
+    /**
+     * @brief Посчитать количество несовершеннолетних
+     * @return Количество жильцов младше 18 (обычные) или 16 (льготники)
+     */
     int countUnderage() const;
 
     void printInfo() const;
